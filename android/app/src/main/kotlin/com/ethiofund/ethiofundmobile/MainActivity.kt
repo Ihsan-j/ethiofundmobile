a65db4deb23bf96276acfe8f58a3e4a562a5108d
Ihsan-j <155774549+Ihsan-j@users.noreply.github.com>
2026-05-30 19:32:21 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/ethiofund/ethiofundmobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/ethiofund/ethiofundmobile/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.ethiofund.ethiofundmobile
 
-// Imports Flutter's Android activity class
+// Import Flutter's Android activity class
 import io.flutter.embedding.android.FlutterActivity
 
 // Main entry point for the Android application.
-// It inherits from FlutterActivity, which launches
-// and hosts the Flutter UI on Android.
+// It inherits from FlutterActivity, which launches and hosts the Flutter UI on Android.
 class MainActivity : FlutterActivity()
